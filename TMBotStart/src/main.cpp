4b398c336cccab2df0a38b7cf033a2477d2da57c
acm233 <SilverSlinky606@outlook.com>
2023-01-14 16:07:41 +0800

    deleted shit mountain

--- TMBotStart/src/main.cpp
+++ TMBotStart/src/main.cpp
@@ -6,18 +6,20 @@ int main()
 {
     if (access("node_modules", F_OK) != 0)
     {
-        char choice;
+        string choice;
         cout << "Requirement Not Found!" << endl;
         cout << "Make sure you`re installed node.js and npm!" << endl;
         cout << "Do you want to install requirement?(Y/n)" << endl;
         cin >> choice;
-        switch (choice)
+        if (choice == "n")
         {
-        case 'n':
             exit(1);
-        default:
-            system("npm i");
         }
+        else
+        {
+            cout << "Installing..." << endl;
+            system("npm i");
+        };
     }
     else
     {
